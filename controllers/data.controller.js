a53a493e8e5d000ca120ba98d6cd2f1c3fb422b2
dathoang997 <dathoang997@gmail.com>
2020-05-06 15:15:18 +0700

    test trade

--- controllers/data.controller.js
+++ controllers/data.controller.js
@@ -342,23 +342,30 @@ module.exports.trade = async function (req, res) {
       })
       array.splice(0, 100)
       if (db_block.number < current_new_block - 7) {
-          let _from_block = Math.max(db_block.number, cursor)
-          let _to_block = Math.min(current_new_block - 6, db_block.number + 5)
-          // console.log('db ' , db_block.number, 'new ' , current_new_block, 'from ' , _from_block,'to ' , _to_block)
-          for (let i = _from_block + 1; i <= _to_block; i++) 
-          array.push(i)
-          processArray(array)
+        let _from_block = Math.max(db_block.number, cursor)
+        let _to_block = Math.min(current_new_block - 6, db_block.number + 5)
+        // console.log('db ' , db_block.number, 'new ' , current_new_block, 'from ' , _from_block,'to ' , _to_block)
+        for (let i = _from_block + 1; i <= _to_block; i++) 
+        array.push(i)
+        processArray(array)
       }
     })
   }
-
   async function processArray(array) {
-    for (const i of array) {
-      await scanBlock(i);
-    }
+    // map array to promises
+    const promises = array.map(scanBlock);
+    // wait until all promises are resolved
+    await Promise.all(promises);
     // console.log('Done!');
     scanOldBlock()
   }
+  // async function processArray(array) {
+  //   for (const i of array) {
+  //     await scanBlock(i);
+  //   }
+  //   // console.log('Done!');
+  //   scanOldBlock()
+  // }
   res.send('collecting...')
 }
 

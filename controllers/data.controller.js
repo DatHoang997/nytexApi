796a2ca02467dcdeb95703cfa43c42dac0f2c2c8
dathoang997 <dathoang997@gmail.com>
2020-05-11 09:27:03 +0700

    test

--- controllers/data.controller.js
+++ controllers/data.controller.js
@@ -1118,7 +1118,7 @@ module.exports.getcandle60 = function (req, res) {
   let num
   Candle.countDocuments({}).exec(async function (err, n) {
     if (err) return handleError(err)
-    num = 41
+    num = doc
   })
   Candle.find({}).sort({time:1}).exec(function (err, doc) {
     if (err) return handleError(err)

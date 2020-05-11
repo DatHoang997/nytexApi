682c6ad23bbeac3290e28376830543ab37bad2d6
dathoang997 <dathoang997@gmail.com>
2020-05-11 09:33:47 +0700

    test

--- controllers/data.controller.js
+++ controllers/data.controller.js
@@ -1161,7 +1161,7 @@ module.exports.getcandle60 = function (req, res) {
           array.push(doc[j].hight, doc[j].low)
           m = m + doc[j].volumeMNTY
           n = n + doc[j].volumeNewSD
-          console.log(j,doc[j].volumeMNTY)
+          console.log(j,doc[j].volumeMNTY,doc[j].volumeMNTY)
         }
         let data = {
           high : Math.max.apply(Math, array),
@@ -1247,6 +1247,7 @@ module.exports.getcandle1 = function (req, res) {
           array.push(doc[j].hight, doc[j].low)
           m = m + doc[j].volumeMNTY
           n = n + doc[j].volumeNewSD
+          console.log(j,doc[j].volumeMNTY,doc[j].volumeMNTY),
         }
         let data = {
           high : Math.max.apply(Math, array),
